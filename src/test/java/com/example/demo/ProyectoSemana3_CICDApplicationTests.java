3c1a47faba87a4920bd936b4164be0a12841cd47
javigon258 <javigon258@gmail.com>
2021-07-30 17:59:21 +0200

    #cambios de test

--- src/test/java/com/example/demo/ProyectoSemana3_CICDApplicationTests.java
+++ src/test/java/com/example/demo/ProyectoSemana3_CICDApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProyectoSemana3MavenV2ApplicationTests {
+class ProyectoSemana3_CICDApplicationTests {
 
 	@Test
 	void contextLoads() {
